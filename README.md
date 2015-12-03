9d307c65045c2c14338e5e8520540eebb6170175
Miroslav Jancarik <miroslav.jancarik@firma.seznam.cz>
2015-12-03 16:52:53 +0100

    #! babel6 polyfill

---
+++ README.md
@@ -0,0 +1,7 @@
+# IMA.js-babel6-polyfill
+
+This repository allows your code written in ES6 will be running IE<=10.
+
+If you are looking for a way to start a new IMA.js application, you should
+follow this link:
+[https://github.com/seznam/IMA.js-skeleton](https://github.com/seznam/IMA.js-skeleton).
---
+++ index.js
@@ -0,0 +1,13 @@
+(function() {
+	if (!(Object.setPrototypeOf || {}.__proto__)) {
+		var nativeGetPrototypeOf = Object.getPrototypeOf;
+
+		Object.getPrototypeOf = function(object) {
+			if (object.__proto__) {
+				return object.__proto__;
+			} else {
+				return nativeGetPrototypeOf.call(Object, object);
+			}
+		}
+	}
+})();
